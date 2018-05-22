2195a93ce7df443e3516858acb70bafc6e67e637
bselles <gabriese@ucm.es>
2018-05-22 11:11:08 +0200

    añadido versión final SVM

--- VERSION2/SVM/entrenarSVM.m
+++ VERSION2/SVM/entrenarSVM.m
@@ -1,12 +1,14 @@
+#Entrenamos el sistema entrenando tantas SVM como etiquetas requiera 
+#el problema (en nuestro caso, 3).
 function accuracy= entrenarSVM(C, TOL,max_passes)
   #Cargamos los datos correspondientes.
   
-  X=load('../datasets/distribucion2/x.csv');
+  X=load('../datasets/x.csv');
   y=X(:,end);
   y=translateClasses(y);
   X=X(:,1:end-1);
   
-  Xval=load('../datasets/distribucion2/xval.csv');
+  Xval=load('../datasets/xval.csv');
   yval=Xval(:,end);
   yval=translateClasses(yval);
   Xval=Xval(:,1:end-1);
@@ -14,7 +16,6 @@ function accuracy= entrenarSVM(C, TOL,max_passes)
   #Entrenamos 3 svm utilizando el esquema OneVsAll.
   for i=1:3
     #Entrenamos el SVM.
-    #model(i)= svmTrain(X,(y==i),C,@(x1,x2) gaussianKernel(x1,x2,sigma));
     model(i)= svmTrain(X,(y==i),C,@linearKernel,TOL,max_passes);
     predictions(:,i)= svmPredict(model(i),Xval);
 
